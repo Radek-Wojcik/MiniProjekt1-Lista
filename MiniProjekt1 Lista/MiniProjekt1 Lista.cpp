ad960281184c1aaf244e514c45174c435a739fce
Radek Wójcik <radekw04@gmail.com>
2025-04-09 15:28:37 +0200

    Stworzenie szkieletu klasy

--- MiniProjekt1 Lista/MiniProjekt1 Lista.cpp
+++ MiniProjekt1 Lista/MiniProjekt1 Lista.cpp
@@ -1,7 +1,24 @@
 ﻿#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+template <typename T>
+class DynamicArray {
+private:
+    T* array;        
+    int capacity;    
+    int length;      
 
+public:
+    // Deklaracja konstruktora i destruktora
+    DynamicArray(int initCapacity = 10);
+    ~DynamicArray();
+
+    // Deklaracja metody dodającej element
+    void push_back(const T& value);
+
+    // Deklaracja operatora umożliwiającego dostęp przez indeks
+    T& operator[](int index);
+
+    // Deklaracja metod pomocniczych
+    int size() const;
+    int getCapacity() const;
+};
