e8cc86b687a31c94a34a00c062dc4238dbd39a36
Radek Wójcik <radekw04@gmail.com>
2025-04-09 15:50:38 +0200

    Implementacja push_back oraz  odwołanie do elementu

--- MiniProjekt1 Lista/MiniProjekt1 Lista.cpp
+++ MiniProjekt1 Lista/MiniProjekt1 Lista.cpp
@@ -1,24 +1,26 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 template <typename T>
 class DynamicArray {
 private:
-    T* array;        
-    int capacity;    
-    int length;      
+    T* array;
+    int capacity;
+    int length;
 
     // Metoda powiększająca tablice
     void resize() {
-        capacity *= 2;                           // Podwojenie pojemności
-        T* newArray = new T[capacity];           // Nowa tablica o zwiększonej pojemności
+        capacity *= 2;                                    // Podwojenie pojemności
+        T* newArray = new T[capacity];                    // Nowa tablica o zwiększonej pojemności
 
         // Kopiowanie elementów do nowej tablicy
         for (int i = 0; i < length; ++i) {
             newArray[i] = array[i];
         }
 
-        delete[] array;                          // Zwolnienie pamięci starej tablicy
-        array = newArray;                        // Przypisanie wskaźnika do nowej tablicy
+        delete[] array;                                   // Zwolnienie pamięci starej tablicy
+        array = newArray;                                 // Przypisanie wskaźnika do nowej tablicy
     }
 
 public:
@@ -26,19 +28,29 @@ public:
     DynamicArray(int initCapacity = 10)
         : capacity(initCapacity), length(0)
     {
-        array = new T[capacity]; 
+        array = new T[capacity];
     }
-    
+
     // Destruktor
-    ~DynamicArray(){
-        delete[] array; 
+    ~DynamicArray() {
+        delete[] array;
     }
 
-    // Deklaracja metody dodającej element
+    // Metoda dodająca element
     void push_back(const T& value);
+    if (length == capacity) {                            // Sprawdzenie, czy tablica jest pełna
+        resize();
+    }
+    array[length++] = value;                             // Dodanie elementu i zwiększenie licznika
 
-    // Deklaracja operatora umożliwiającego dostęp przez indeks
-    T& operator[](int index);
+    // Zabezpieczenie przed odowłaniem do elementu spoza zakresu
+    T& operator[](int index) {
+        if (index < 0 || index >= length) {              // Sprawdzenie, czy indeks mieści się w zakresie
+            cerr << "Indeks poza zakresem!" << endl;     // Błąd krytyczny, zakończenie progrsamu
+            exit(EXIT_FAILURE);
+        }
+        return array[index];
+    }
 
     // Deklaracja metod pomocniczych
     int size() const;
